fe661e7c27ad9eb29ccc9be7f8c0932c545f71b6
brandon <brandon4261@gmail.com>
2016-07-17 18:53:28 +1000

    Ok im just done with broken proxies!

--- src/main/java/com/brandon3055/brandonscore/proxy/ClientProxy.java
+++ src/main/java/com/brandon3055/brandonscore/proxy/ClientProxy.java
@@ -5,9 +5,6 @@ import com.brandon3055.brandonscore.client.particle.BCEffectHandler;
 import com.brandon3055.brandonscore.utils.ModelUtils;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.resources.IReloadableResourceManager;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
@@ -24,38 +21,38 @@ public class ClientProxy extends CommonProxy {
         MinecraftForge.EVENT_BUS.register(new BCClientEventHandler());
     }
 
-    @Override
-    public boolean isDedicatedServer() {
-        return false;
-    }
-
-    @Override
-    public MinecraftServer getMCServer() {
-        return super.getMCServer();
-    }
-
-    @Override
-    public World getClientWorld() {
-        return Minecraft.getMinecraft().theWorld;
-    }
-
-    @Override
-    public boolean isJumpKeyDown() {
-        return Minecraft.getMinecraft().gameSettings.keyBindJump.isKeyDown();
-    }
-
-    @Override
-    public boolean isSneakKeyDown() {
-        return Minecraft.getMinecraft().gameSettings.keyBindSneak.isKeyDown();
-    }
-
-    @Override
-    public boolean isSprintKeyDown() {
-        return Minecraft.getMinecraft().gameSettings.keyBindSprint.isKeyDown();
-    }
-
-    @Override
-    public EntityPlayer getClientPlayer() {
-        return Minecraft.getMinecraft().thePlayer;
-    }
+//    @Override
+//    public boolean isDedicatedServer() {
+//        return false;
+//    }
+//
+//    @Override
+//    public MinecraftServer getMCServer() {
+//        return super.getMCServer();
+//    }
+//
+//    @Override
+//    public World getClientWorld() {
+//        return Minecraft.getMinecraft().theWorld;
+//    }
+//
+//    @Override
+//    public boolean isJumpKeyDown() {
+//        return Minecraft.getMinecraft().gameSettings.keyBindJump.isKeyDown();
+//    }
+//
+//    @Override
+//    public boolean isSneakKeyDown() {
+//        return Minecraft.getMinecraft().gameSettings.keyBindSneak.isKeyDown();
+//    }
+//
+//    @Override
+//    public boolean isSprintKeyDown() {
+//        return Minecraft.getMinecraft().gameSettings.keyBindSprint.isKeyDown();
+//    }
+//
+//    @Override
+//    public EntityPlayer getClientPlayer() {
+//        return Minecraft.getMinecraft().thePlayer;
+//    }
 }
--- src/main/java/com/brandon3055/brandonscore/proxy/CommonProxy.java
+++ src/main/java/com/brandon3055/brandonscore/proxy/CommonProxy.java
@@ -1,9 +1,5 @@
 package com.brandon3055.brandonscore.proxy;
 
-import net.minecraftforge.fml.common.FMLCommonHandler;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.world.World;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 /**
@@ -15,42 +11,42 @@ public class CommonProxy {
 
     }
 
-    public boolean isDedicatedServer() {
-        return true;
-    }
-
-    public MinecraftServer getMCServer() {
-        return FMLCommonHandler.instance().getMinecraftServerInstance();
-    }
-
-    public World getClientWorld() {
-        return null;
-    }
-
-    public boolean isOp(String paramString) {
-        MinecraftServer localMinecraftServer = FMLCommonHandler.instance().getMinecraftServerInstance();
-        paramString = paramString.trim();
-        for (String str : localMinecraftServer.getPlayerList().getOppedPlayerNames()) {
-            if (paramString.equalsIgnoreCase(str)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public boolean isJumpKeyDown() {
-        return false;
-    }
-
-    public boolean isSprintKeyDown() {
-        return false;
-    }
-
-    public boolean isSneakKeyDown() {
-        return false;
-    }
-
-    public EntityPlayer getClientPlayer() {
-        return null;
-    }
+//    public boolean isDedicatedServer() {
+//        return true;
+//    }
+//
+//    public MinecraftServer getMCServer() {
+//        return FMLCommonHandler.instance().getMinecraftServerInstance();
+//    }
+//
+//    public World getClientWorld() {
+//        return null;
+//    }
+//
+//    public boolean isOp(String paramString) {
+//        MinecraftServer localMinecraftServer = FMLCommonHandler.instance().getMinecraftServerInstance();
+//        paramString = paramString.trim();
+//        for (String str : localMinecraftServer.getPlayerList().getOppedPlayerNames()) {
+//            if (paramString.equalsIgnoreCase(str)) {
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
+//
+//    public boolean isJumpKeyDown() {
+//        return false;
+//    }
+//
+//    public boolean isSprintKeyDown() {
+//        return false;
+//    }
+//
+//    public boolean isSneakKeyDown() {
+//        return false;
+//    }
+//
+//    public EntityPlayer getClientPlayer() {
+//        return null;
+//    }
 }
