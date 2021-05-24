cade6a8cb76d668f15252b0324272f44174b0162
brandon3055 <brandon4261@gmail.com>
2021-05-24 18:02:51 +1000

    Added some generic math utils

--- src/main/java/com/brandon3055/brandonscore/utils/MathUtils.java
+++ src/main/java/com/brandon3055/brandonscore/utils/MathUtils.java
@@ -1,5 +1,8 @@
 package com.brandon3055.brandonscore.utils;
 
+import codechicken.lib.math.MathHelper;
+import codechicken.lib.vec.Vector3;
+
 import java.util.Random;
 
 /**
@@ -22,7 +25,7 @@ public class MathUtils {
      * This is a 'random' float generator.
      * - It actually just loops through a set of randSet.length different random floats
      * Much faster than java random and more than sufficient for certain rendering tasks.
-     * */
+     */
     public static float nextFloat() {
         return randSet[randPos++ % randSet.length];
     }
@@ -31,9 +34,9 @@ public class MathUtils {
      * Sets the current position for 'random' float generator.
      * Effectively the same function as {@link Random#setSeed(long)} because the floats
      * will always be supplied in the same order.
-     * */
+     */
     public static void setRandSeed(long i) {
-        randPos = (int)i % randSet.length;
+        randPos = (int) i % randSet.length;
     }
 
     /**
@@ -57,6 +60,10 @@ public class MathUtils {
         return (valueIn - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
     }
 
+    public static double clampMap(double valueIn, double inMin, double inMax, double outMin, double outMax) {
+        return autoClamp(map(valueIn, inMin, inMax, outMin, outMax), outMin, outMax);
+    }
+
     public static int getNearestMultiple(int number, int multiple) {
         int result = number;
 
@@ -71,19 +78,33 @@ public class MathUtils {
         return result;
     }
 
+    @Deprecated
     public static int clamp(int num, int min, int max) {
         return Math.max(min, Math.min(num, max));
     }
 
+    @Deprecated
     public static float clamp(float num, float min, float max) {
         return Math.max(min, Math.min(num, max));
     }
 
+    @Deprecated
     public static double clamp(double num, double min, double max) {
         return Math.max(min, Math.min(num, max));
     }
 
+    @Deprecated
     public static long clamp(long num, long min, long max) {
         return Math.max(min, Math.min(num, max));
+    }
+
+    public static double autoClamp(double value, double boundA, double boundB) {
+        return MathHelper.clip(value, Math.min(boundA, boundB), Math.max(boundA, boundB));
+    }
+
+    public static Vector3 interpolateVec3(Vector3 from, Vector3 to, double position) {
+        return new Vector3(from.x + (to.x - from.x) * position,
+                from.y + (to.y - from.y) * position,
+                from.z + (to.z - from.z) * position);
     }
 }
